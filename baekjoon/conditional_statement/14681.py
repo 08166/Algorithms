e8b9a25b82eac347ceb8b6c9375ae854a057ca8b
08166 <j76067469@gmail.com>
2024-10-07 18:08:49 +0900

    feat:2024/10/24

--- baekjoon/conditional_statement/14681.py
+++ baekjoon/conditional_statement/14681.py
@@ -31,3 +31,16 @@ C (-12, -5) |  D (12, -5)
 - 예제 출력 2
 4
 """
+
+
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0 :
+    print("1")
+elif x < 0 and y > 0 :
+    print("2")
+elif x < 0 and y < 0 :
+    print("3")
+elif x > 0 and y < 0 :
+    print("4")
